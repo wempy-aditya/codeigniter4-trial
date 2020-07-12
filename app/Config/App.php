5d48d3819665b86adbdcaa88167a01663343d86a
Wempy Aditya Wiryawan <67529099+wempy-aditya@users.noreply.github.com>
2020-07-12 10:58:42 +0700

    update app.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -28,9 +28,7 @@ class App extends BaseConfig
 	| Index File
 	|--------------------------------------------------------------------------
 	|
-	| Typically this will be your index.php file, unless you've renamed it to
-	| something else. If you are using mod_rewrite to remove the page set this
-	| variable so that it is blank.
+	| Typically this will be your index.php file, unless you
 	|
 	*/
 	public $indexPage = '';
